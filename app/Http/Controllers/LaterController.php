6b0c727eefd7a9592cd7d4b7720ad3893a57e309
Abdul Basit <basitawan.abdul@gmail.com>
2022-10-05 10:21:53 +0500

    updated code

---
+++ app/Http/Controllers/LaterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Log;
+use Illuminate\Support\Facades\Storage;
+
+class LaterController extends Controller
+{
+    public function pics ($Code)
+    {
+       $photos = json_decode( Storage::disk('local')->get($Code . '.json') );
+       
+        #echo var_dump ($photos); exit;
+
+        return view('later_pics', compact('photos'));
+    }
+
+
+}
---
+++ resources/views/later_pics.blade.php
@@ -0,0 +1,297 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+      <head>
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        
+          <!-- Global site tag (gtag.js) - Google Ads: 10851736857 -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10851736857"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-10851736857');
+
+            gtag('event', 'conversion', {'send_to': 'AW-10851736857/GASnCJH4uaADEJm6wbYo'});
+          </script>
+
+          <script type="text/javascript">
+          window._mfq = window._mfq || [];
+          (function() {
+            var mf = document.createElement("script");
+            mf.type = "text/javascript"; mf.defer = true;
+            mf.src = "//cdn.mouseflow.com/projects/b1b3086b-69d7-4801-9797-142f5c8dc726.js";
+            document.getElementsByTagName("head")[0].appendChild(mf);
+          })();
+        </script>
+
+
+                <title>
+            
+             Yoda | Later Cam             
+
+        </title>
+
+
+                  <style>
+            @import  url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+          </style>
+
+          <script src="https://unpkg.com/cursor-effects@latest/dist/browser.js"></script>
+
+        
+        
+    <script src="/js/masonry.pkgd.min.js"></script>
+    
+
+
+                  
+          <!-- begin olark code -->
+<script type="text/javascript">
+;(function(o,l,a,r,k,y){if(o.olark)return;
+r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
+y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
+y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
+y.extend=function(i,j){y("extend",i,j)};
+y.identify=function(i){y("identify",k.i=i)};
+y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
+k=y._={s:[],t:[+new Date],c:{},l:a};
+})(window,document,"static.olark.com/jsclient/loader.js");
+/* Add configuration calls below this comment */
+olark.identify('6935-124-10-4396');</script>
+<!-- end olark code -->
+        
+       
+                  <link rel="stylesheet" href="/css/home.css?v=4">
+        
+       
+
+                  <link rel="stylesheet" href="/css/app.css?v=4">
+        
+
+                  <link rel="stylesheet" href="/css/latercam.css?v=4">
+
+                
+
+
+                    <meta name="twitter:card" content="summary" />
+        
+                  <meta name="twitter:description" content="Later Cam turns your phone into a disposable camera." />
+          <meta name="twitter:url" content="http://cctrphwvh.ngrok.io/pics/YNtyI2412H" />
+
+          
+                
+        
+
+                      <meta property="og:image" content="https://herman-cat.s3.amazonaws.com/later/90s_2.png" />
+
+                  
+       
+
+
+         
+
+
+
+        
+
+          <meta property="og:title" content=" Yoda | Later Cam " />
+         
+          <meta property="og:description" content="Text your photos and we&#039;ll mail 4x6 prints to your grandparents" />
+
+        
+       
+        <meta property="og:type" content="website" />
+        
+        
+        
+              <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/favicon/latercam/apple-touch-icon-57x57.png" />
+              <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/favicon/latercam/apple-touch-icon-114x114.png" />
+              <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/favicon/latercam/apple-touch-icon-72x72.png" />
+              <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/favicon/latercam/apple-touch-icon-144x144.png" />
+              <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/favicon/latercam/apple-touch-icon-60x60.png" />
+              <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/favicon/latercam/apple-touch-icon-120x120.png" />
+              <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/favicon/latercam/apple-touch-icon-76x76.png" />
+              <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/favicon/latercam/apple-touch-icon-152x152.png" />
+              <link rel="icon" type="image/png" href="/favicon/latercam/favicon-196x196.png" sizes="196x196" />
+              <link rel="icon" type="image/png" href="/favicon/latercam/favicon-96x96.png" sizes="96x96" />
+              <link rel="icon" type="image/png" href="/favicon/latercam/favicon-32x32.png" sizes="32x32" />
+              <link rel="icon" type="image/png" href="/favicon/latercam/favicon-16x16.png" sizes="16x16" />
+              <link rel="icon" type="image/png" href="/favicon/latercam/favicon-128.png" sizes="128x128" />
+              <meta name="application-name" content="&nbsp;"/>
+              <meta name="msapplication-TileColor" content="#FFFFFF" />
+              <meta name="msapplication-TileImage" content="/favicon/latercam/mstile-144x144.png" />
+              <meta name="msapplication-square70x70logo" content="/favicon/latercam/mstile-70x70.png" />
+              <meta name="msapplication-square150x150logo" content="/favicon/latercam/mstile-150x150.png" />
+              <meta name="msapplication-wide310x150logo" content="/favicon/latercam/mstile-310x150.png" />
+              <meta name="msapplication-square310x310logo" content="/favicon/latercam/mstile-310x310.png" />
+
+
+            
+        
+        
+        
+
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        
+        <meta name="csrf-token" content="XhGYO5ohP5RMjcyddh0wVE9qZM9yAyntxO9j9kgB" />
+
+        <script type="text/javascript">
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            function copyToClipboard(element) {
+                var $temp = $("<input>");
+                $("body").append($temp);
+                $temp.val($(element).text()).select();
+                document.execCommand("copy");
+                $temp.remove();
+            }
+        </script>
+
+       
+          <script type="text/javascript">
+          (function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script");r.type="text/javascript";
+          r.async=true;r.src="https://d24n15hnbwhuhn.cloudfront.net/libs/amplitude-3.4.1-min.gz.js";
+          r.onload=function(){if(e.amplitude.runQueuedFunctions){e.amplitude.runQueuedFunctions();
+          }else{console.log("[Amplitude] Error: could not load SDK")}};var i=t.getElementsByTagName("script")[0];
+          i.parentNode.insertBefore(r,i);function o(e,t){e.prototype[t]=function(){this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));
+          return this}}var s=function(){this._q=[];return this};var a=["add","append","clearAll","prepend","set","setOnce","unset"];
+          for(var u=0;u<a.length;u++){o(s,a[u])}n.Identify=s;var c=function(){this._q=[];return this;
+          };var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"];
+          for(var p=0;p<l.length;p++){o(c,l[p])}n.Revenue=c;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","logEventWithTimestamp","logEventWithGroups"];
+          function v(e){function t(t){e[t]=function(){e._q.push([t].concat(Array.prototype.slice.call(arguments,0)));
+          }}for(var n=0;n<d.length;n++){t(d[n])}}v(n);n.getInstance=function(e){e=(!e||e.length===0?"$default_instance":e).toLowerCase();
+          if(!n._iq.hasOwnProperty(e)){n._iq[e]={_q:[]};v(n._iq[e])}return n._iq[e]};e.amplitude=n;
+          })(window,document);
+
+          amplitude.getInstance().init("fd6184ebf97b820b613f21909ac5fc74", null, { includeUtm: true, includeReferrer: true });
+
+        </script>
+
+      
+
+       <script type="text/javascript">
+
+          $( document ).ready(function() {
+              
+
+              
+
+                var container = document.querySelector('#masonry-grid');
+
+                var msnry = new Masonry( container, {
+                   itemSelector: '.grid-item',
+                   columnWidth: '.grid-sizer',
+                   percentPosition: true
+                });    
+
+                
+
+
+          });
+          
+        </script>
+      
+
+    </head>
+    
+    <body class="later">
+        
+
+                
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-4">
+                            
+
+
+                        </div>
+
+                        <div class="laterbox col-lg-4 text-center">
+
+                            <a href="/"><img class="mt-4 later_logo" alt="Later Cam logo" src="https://s3.amazonaws.com/herman-cat/latercam.png"/></a>
+                            <h1>Album title</h1>
+
+                            
+                        </div>
+
+                        <div class="col-lg-4"></div>
+
+                    </div>
+                </div>
+
+
+                
+
+                
+                <div class="container">
+                    <div class="grid grid--fluid-5-col" id="masonry-grid">
+
+                        <div class="grid-sizer"></div>
+                        <?php 
+
+                            $i = 0;
+
+                            foreach ($photos as $photo):
+
+                            ?>
+
+
+                            <?php /*@if ($photo->height < $photo->width && !$photo->is_square()) 
+                                        @if ($i != 0 && $i%10==0) grid-item--width4 @else grid-item--width2 @endif @elseif ($i%10==0) grid-item--height4 @endif"><img class="lazy" src="{{$photo->active_image_url(false,true)}}" /></div> */ ?>
+
+                            
+                                <div class="grid-item 
+
+                                    @if ($photo->height < $photo->width) grid-item--width2 @endif"><img class="lazy" src="{{$photo->web}}" /></div>
+                             
+                            
+
+                            
+                                
+                            
+
+                            <?php $i++ ?>
+
+                        <?php endforeach;?>
+
+                                                
+                    </div>
+                </div>
+
+                
+
+                <div class="text-center" style="margin-top: 300px;">
+                    <a href="/privacy">Privacy</a>
+                </div>
+
+        
+    </body>
+
+
+<footer>
+    <script>window.__theme = 'bs4';</script>
+    
+        
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+   
+
+
+    
+   
+</footer>
+</html>
